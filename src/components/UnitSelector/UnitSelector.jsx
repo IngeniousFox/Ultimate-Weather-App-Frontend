a792e008698e187281fc32b7787917344a9de77c
Jonathan Raya Rios <jonathan17699@hotmail.es>
2022-07-28 11:49:35 +0200

    Disable button when her unit it's selected

--- src/components/UnitSelector/UnitSelector.jsx
+++ src/components/UnitSelector/UnitSelector.jsx
@@ -1,13 +1,16 @@
 import styles from "./UnitSelector.module.css"
+import {useSelector} from "react-redux";
 
 export const UnitSelector = () => {
 
     //TODO: Add functionality reload the entire app when changing the units and update the active class
 
+    const {units} = useSelector(state => state.weather)
+
 	return (
 		<div>
-            <button className={`${styles.unit} ${styles.active}`}>Metric</button>
-            <button className={`${styles.unit}`}>Imperial</button>
+            <button className={`${styles.unit} ${(units === "metric") ? styles.active : ""}`} disabled={units === "metric"} onClick={() => console.log("metric")}>Metric</button>
+            <button className={`${styles.unit} ${(units === "imperial") ? styles.active : ""}`} disabled={units === "imperial"} onClick={() => console.log("imperial")}>Imperial</button>
         </div>
 	)
 }
